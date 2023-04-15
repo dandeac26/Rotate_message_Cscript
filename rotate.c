c6a11de0250942366998957d76d058d1e6ef6e2b
Dan Cristian Deac <79625820+dandeac26@users.noreply.github.com>
2023-04-15 05:17:38 +0300

    finishing touches

--- rotate.c
+++ rotate.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 #define BUFSIZE 100
 #define max_upper 90
@@ -9,11 +10,10 @@
 #define min_upper 65
 #define min_lower 97
 
-int check_num(char *num){ /// check if argv[2] is a number and if so, return it through r
+int check_num(char *num){
     unsigned int r = 0;
     int p = 1;
     for(int i = strlen(num)-1; i >= 0;i--){
-        
         if(isdigit(num[i])==0){
             printf("Error: non digit characters\n");
             return -1;
@@ -25,14 +25,21 @@ int check_num(char *num){ /// check if argv[2] is a number and if so, return it 
     return r;
 }
 
+bool notValid(char *arr){
+    if(strcmp(arr, "-b")==0 || strcmp(arr, "-f")==0 || 
+       strcmp(arr, "--backward")==0 || 
+       strcmp(arr, "--forward")==0)
+        return false;
+    return true;
+}
+
 int main(int argc, char **argv){
-    if(argc == 2 && strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0){
+    if(argc >= 2 && strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0){
         printf("\n      -- ROT encoder/decoder -- \n\n    Program expects two arguments:\n      *direction: {forward,backward} or {-f, -b}\n      *number to rotate by\n\n    Example for rotating forward by 10: ./rotate -f 10\n\n    Program expects a string to be entered - no spaces as of this implementention are allowed\n");
         return 0;
     }
-    
-    if(argc < 3){
-        printf("\nProgram expects two arguments:\n    *direction: {forward,backward} or {-f, -b}\n    *number to rotate by\n\nUse --help for more info.\n\n");
+    if(argc < 3 || notValid(argv[1])){
+        printf("\nProgram expects two arguments:\n    *direction: {forward,backward} or {-f, -b}\n    *number to rotate by\n    (message)\n\nUse --help for more info.\n\n");
         return 1;
     }
 
@@ -46,24 +53,31 @@ int main(int argc, char **argv){
     
     if(ROT < 1 || ROT > 25){
         printf("Invalid ROT value\n-has to be between 1-25\n");
-        return 3;
+        return 2;
     }
 
     /// handling direction of rotation
-    if(strcmp(argv[1], "forward") == 0 || strcmp(argv[1], "-f") == 0)
-        printf("    --Rot-%d forward --\nEnter message: ", ROT);
-    else if(strcmp(argv[1], "backward") == 0 || strcmp(argv[1], "-b") == 0){
-        printf("    --Rot-%d backward --\nEnter message: ", ROT);
+    char dir[10];
+    if(strcmp(argv[1], "--forward") == 0 || strcmp(argv[1], "-f") == 0)
+        strcpy(dir,"forward");
+    else if(strcmp(argv[1], "--backward") == 0 || strcmp(argv[1], "-b") == 0){
+        strcpy(dir,"backward");
         ROT = 26 - ROT; // use the complement 
     }
-       
-    scanf("%s", buf);  /// read string ( NO SPACES !) 
+
+    printf("\n------ Rot-%d %s ------\n", ROT, dir);
+    
+    if(argv[3]){
+        strcpy(buf,argv[3]);
+    }else{
+        printf("Enter message: "); 
+        scanf("%s", buf);
+    }
 
     int decimal_value = -1;
-    printf("result msg   : ");
-    
+    printf("\nresult: ");
     for(int i = 0; i<strlen(buf); i++){
-        if(isalpha(buf[i])){
+        if(!isalpha(buf[i])){
             printf("%c", buf[i]);
             continue;
         }
@@ -80,6 +94,7 @@ int main(int argc, char **argv){
             printf("%c", (char)(decimal_value + ROT));
         
     }
-    printf("\nCompleted Successfully\n");
+    printf("\n\n<<Completed Successfully>>\n");
     return 0;
 }
+
